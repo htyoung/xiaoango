b31e7c90dc00799aebd78ecd2d9d97e8c2c022f5
htyoung <hty551020@hotmail.com>
2017-12-03 15:59:16 +0800

    format code

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,39 +2,39 @@
 //获取应用实例
 var app = getApp()
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {}
-  },
-  //事件处理函数
-  bindViewTap: function () {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  catchBtnClickme: function () {
-    var title = []
-    var userInfo = this.data.userInfo
-    if (userInfo.nickName == '小安') {
-      title.unshift("老板，你好！")
-    }else{
-      title.unshift("员工，你好！")
+    data: {
+        motto: 'Hello World',
+        userInfo: {}
+    },
+    //事件处理函数
+    bindViewTap: function () {
+        wx.navigateTo({
+            url: '../logs/logs'
+        })
+    },
+    catchBtnClickme: function () {
+        var title = []
+        var userInfo = this.data.userInfo
+        if (userInfo.nickName == '小安') {
+            title.unshift("老板，你好！")
+        } else {
+            title.unshift("员工，你好！")
+        }
+        wx.showToast({
+            title: title.toString(),
+            icon: 'success',
+            duration: 500
+        });
+    },
+    onLoad: function () {
+        console.log('onLoad')
+        var that = this
+        //调用应用实例的方法获取全局数据
+        app.getUserInfo(function (userInfo) {
+            //更新数据
+            that.setData({
+                userInfo: userInfo
+            })
+        })
     }
-    wx.showToast({
-      title: title.toString(),
-      icon: 'success',
-      duration: 500
-    });
-  },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function (userInfo) {
-      //更新数据
-      that.setData({
-        userInfo: userInfo
-      })
-    })
-  }
 })
